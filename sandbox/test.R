43ec73a3d88b92f5af2a385b0c0b8d3ad8ad7620
Rodo-ASUS <rodo.nunez.u@gmail.com>
2022-08-23 17:29:50 -0400

    estructura inicial

---
+++ sandbox/test.R
@@ -0,0 +1,15 @@
+lintr::use_lintr(type = "tidyverse")
+usethis::use_github_action("lint-project")
+lintr::lint_dir()
+
+
+# Seccion ----------
+
+plot(mtcars$mpg)
+
+suppressWarnings(library(tidyverse))
+
+# Seccion --------------------------------
+
+ # Section ---------------------------------------- 
+ # Section ---------------------------------------- 
